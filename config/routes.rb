86d6135fbfa661073d8b4399c99bada6afe39a61
Kathrin Wendt <kathrinwendt2908@gmail.com>
2020-04-02 17:12:25 +0200

    made footer sticky, created dashboard page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  get 'categories/new'
-  get 'categories/edit'
-  get 'products/show'
-  get 'products/new'
-  get 'products/edit'
   devise_for :users
   root to: 'pages#home'
   resources :products, only: [:show, :new, :create, :edit, :update, :destroy]
+  get 'dashboard', to: 'pages#dashboard'
 end
