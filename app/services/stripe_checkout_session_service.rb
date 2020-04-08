3b0035e1b720594b17ac12995b9f00b834c01e7c
Kathrin Wendt <kathrinwendt2908@gmail.com>
2020-04-08 17:08:13 +0200

    added view of orders#index to show details of past orders

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -3,6 +3,5 @@ class StripeCheckoutSessionService
     order = Order.find_by(checkout_session_id: event.data.object.id)
     order.update(state: 'paid')
     order.user.shopping_bag.destroy
-    ShoppingBag.create(user: current_user)
   end
 end
