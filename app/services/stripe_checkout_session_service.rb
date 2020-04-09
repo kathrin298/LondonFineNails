29a7fb05a623287ad7018e95d7d9fe1b668bd894
Kathrin Wendt <kathrinwendt2908@gmail.com>
2020-04-09 17:57:31 +0200

    added icon with number of items in shopping bag to navbar

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -3,5 +3,6 @@ class StripeCheckoutSessionService
     order = Order.find_by(checkout_session_id: event.data.object.id)
     order.update(state: 'paid')
     order.user.shopping_bag.destroy
+    ShoppingBag.create(user: order.user)
   end
 end
