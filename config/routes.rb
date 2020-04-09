fca327c7589183b9e85355432ef58d045e6d54f0
Kathrin Wendt <kathrinwendt2908@gmail.com>
2020-04-09 18:57:40 +0200

    initiated instance of shopping bag when user registers, shopping bag count icon responsive to position of shopping bag item in dropdown, fixed product show page so that only products not in bag can be added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  #devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   root to: 'pages#home'
   get 'dashboard', to: 'pages#dashboard'
   resources :categories, only: [:new, :create, :edit, :update, :destroy]
