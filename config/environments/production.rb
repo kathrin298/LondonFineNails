bebfd0750e53b4607d3dac309f831b0b5e427778
Kathrin Wendt <kathrinwendt2908@gmail.com>
2020-04-15 18:28:11 +0200

    changed env email and password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,19 +70,6 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
 
   config.action_mailer.default_url_options = { :host => 'londonfinenails.herokuapp.com' }
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.perform_deliveries = true
-  # config.action_mailer.raise_delivery_errors = false
-  # config.action_mailer.default :charset => "utf-8"
-  # config.action_mailer.smtp_settings = {
-  #   address:              'smtp.gmail.com',
-  #   port:                 587,
-  #   domain:               'londonfinenails.herokuapp.com',
-  #   user_name:            ENV["GMAIL_EMAIL"],
-  #   password:             ENV["GMAIL_PASSWORD"],
-  #   authentication:       'plain',
-  #   enable_starttls_auto: true  }
-
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
